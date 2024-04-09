51dfa1dc505cb2b37eb834cfef02f1900e67eb35
Giulio <giulio.carpi-lapi@etu.unistra.fr>
2024-04-09 16:47:16 +0200

    update

--- include/tree.hpp
+++ include/tree.hpp
@@ -16,9 +16,13 @@ public:
     double circumference;
     double diameter_crown;
 
+    // Default constructor
+    Tree();
+
+    // Constructor that takes arguments
     Tree(long id, double lat, double lon, std::string genus, std::string species, double height, double circumference, double diameter_crown);
 };
 
-Tree createTreeFromJson(nlohmann::json treeJson);
+Tree createTreeFromJson(const nlohmann::json& treeJson);
 
 #endif
