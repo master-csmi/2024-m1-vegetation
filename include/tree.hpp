164c952730e84475ba14fc88e399a40462fdf644
PA Senger <pierre.antoine.senger@gmail.com>
2024-04-20 21:35:33 +0200

    member name and getters

--- include/tree.hpp
+++ include/tree.hpp
@@ -1,37 +1,32 @@
 #ifndef TREE_HPP
 #define TREE_HPP
 
-#include "WGS84toCartesian.hpp"
 #include "json.hpp"
-#include "query.hpp"
 #include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
 #include <CGAL/IO/polygon_soup_io.h>
 #include <CGAL/Polygon_mesh_processing/bbox.h>
 #include <CGAL/Real_timer.h>
 #include <CGAL/Surface_mesh.h>
 #include <CGAL/alpha_wrap_3.h>
-#include <array>
 #include <iostream>
 #include <string>
-#include <vector>
 
 using K = CGAL::Exact_predicates_inexact_constructions_kernel;
 using Point_3 = K::Point_3;
 using Mesh = CGAL::Surface_mesh<Point_3>;
 
-class TreeMesh;
 class Tree {
   private:
-    long id;
-    double lat;
-    double lon;
-    std::string genus;
-    std::string species;
-    std::string season;
-    double height;
-    double circumference;
-    double diameter_crown;
-    double x, y;
+    long M_id;
+    double M_lat;
+    double M_lon;
+    std::string M_genus;
+    std::string M_species;
+    std::string M_season;
+    double M_height;
+    double M_circumference;
+    double M_diameter_crown;
+    double M_x, M_y;
     Mesh M_wrap;
 
   public:
@@ -44,44 +39,39 @@ class Tree {
          double diameter_crown);
 
     // Getters
-    long getId() const { return id; }
-    double getLat() const { return lat; }
-    double getLon() const { return lon; }
-    std::string getGenus() const { return genus; }
-    std::string getSpecies() const { return species; }
-    double getHeight() const { return height; }
-    double getCircumference() const { return circumference; }
-    double getDiameterCrown() const { return diameter_crown; }
-    std::string getSeason() const { return season; }
-    double getX() const { return x; }
-    double getY() const { return y; }
-    Mesh getWrap() const { return M_wrap; }
+    long id() const { return M_id; }
+    double lat() const { return M_lat; }
+    double lon() const { return M_lon; }
+    std::string genus() const { return M_genus; }
+    std::string species() const { return M_species; }
+    double height() const { return M_height; }
+    double circumference() const { return M_circumference; }
+    double diameterCrown() const { return M_diameter_crown; }
+    std::string season() const { return M_season; }
+    double x() const { return M_x; }
+    double y() const { return M_y; }
+    Mesh wrap() const { return M_wrap; }
 
     // Setters
-    void setId(long id) { this->id = id; }
-    void setLat(double lat) { this->lat = lat; }
-    void setLon(double lon) { this->lon = lon; }
-    void setGenus(std::string genus) { this->genus = genus; }
-    void setSpecies(std::string species) { this->species = species; }
-    void setHeight(double height) { this->height = height; }
+    void setId(long id) { M_id = id; }
+    void setLat(double lat) { M_lat = lat; }
+    void setLon(double lon) { M_lon = lon; }
+    void setGenus(std::string genus) { M_genus = genus; }
+    void setSpecies(std::string species) { M_species = species; }
+    void setHeight(double height) { M_height = height; }
     void setCircumference(double circumference) {
-        this->circumference = circumference;
+        M_circumference = circumference;
     }
     void setDiameterCrown(double diameter_crown) {
-        this->diameter_crown = diameter_crown;
+        M_diameter_crown = diameter_crown;
     }
-    void computeXY(double ref_lat, double ref_lon) {
-        std::array<double, 2> cartesianPosition =
-            wgs84::toCartesian({ref_lat, ref_lon} /* reference position */,
-                               {lon, lat} /* position to be converted */);
-        x = cartesianPosition[0];
-        y = cartesianPosition[1];
-    }
-    void wrap(int lod);
+    void setSeason(std::string season) { M_season = season; }
 
-    friend std::ostream &operator<<(std::ostream &os, const Tree &tree);
+    void computeXY(double ref_lat, double ref_lon);
+    void wrap(int lod);
 };
 
 Tree createTreeFromJson(const nlohmann::json &treeJson);
+std::ostream &operator<<(std::ostream &os, const Tree &tree);
 
 #endif
