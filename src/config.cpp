621778ec4446719f88b24394df5346009be43058
PA Senger <pierre.antoine.senger@gmail.com>
2024-05-19 21:15:38 +0200

    cleaned api part

--- src/config.cpp
+++ src/config.cpp
@@ -8,10 +8,7 @@ Config::Config(std::string const &filename) {
     std::ifstream ifile(filename);
     if (ifile) {
         nlohmann::json data = nlohmann::json::parse(ifile);
-        A_lat = data["A"]["latitude"];
-        A_lon = data["A"]["longitude"];
-        B_lat = data["B"]["latitude"];
-        B_lon = data["B"]["longitude"];
+        M_bbox = data["bbox"];
         M_LOD = data["LOD"];
         M_output_name = data["output_name"];
         M_default_genus = data["default_genus"];
@@ -23,19 +20,9 @@ Config::Config(std::string const &filename) {
 
 Config::~Config() {}
 
-double Config::Alat() const { return A_lat; }
-double Config::Alon() const { return A_lon; }
-double Config::Blat() const { return B_lat; }
-double Config::Blon() const { return B_lon; }
-double Config::LOD() const { return M_LOD; }
-Query Config::query() const { return Query(A_lat, A_lon, B_lat, B_lon); }
-std::string Config::output_name() const { return M_output_name; }
-
 std::ostream &operator<<(std::ostream &os, const Config &config) {
-    os << std::setprecision(10) << "A lat: " << config.Alat()
-       << ", A lon: " << config.Alon() << std::endl;
-    os << std::setprecision(10) << "B lat: " << config.Blat()
-       << ", B lon: " << config.Blon() << std::endl;
+    os << std::setprecision(10) << "Bounding box: " << config.bbox()
+       << std::endl;
     os << "LOD: " << config.LOD() << std::endl;
     return os;
 }
