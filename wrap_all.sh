c2b468e231a802f1845ca95503e99580320391d5
PA Senger <pierre.antoine.senger@gmail.com>
2024-05-19 23:19:30 +0200

    updated readme and report

--- wrap_all.sh
+++ wrap_all.sh
@@ -3,19 +3,10 @@
 # Directory containing the raw STL files
 INPUT_DIR="tree_ref/raw_tree"
 
-# Scaling factor
-SCALING_FACTOR=100
-
-# Output directory for the wrapped STL files
-OUTPUT_DIR="tree_ref/"
+# The alpha value to use for wrapping
+ALPHA=100
 
 # Loop through all STL files in the input directory
 for input_file in $INPUT_DIR/*.stl; do
-	# Extract the filename without the extension
-
-	# Define the output filename
-
-	# Run the wrap program
-	./build/wrap "$input_file" "$SCALING_FACTOR"
-
+	./build/wrap "$input_file" "$ALPHA"
 done
