d78e4f595c22121f75122f99f9ca8a8276266e29
PA Senger <pierre.antoine.senger@gmail.com>
2024-05-25 00:22:45 +0200

    test new ci

--- src/tree.cpp
+++ src/tree.cpp
@@ -85,8 +85,8 @@ void Tree::load_data(const std::string &filename) {
     }
 }
 
-void Tree::wrap(int lod) {
-    std::string filename = "tree_ref/";
+void Tree::wrap(int lod, std::string rep) {
+    std::string filename = rep + "tree_ref/";
     double scaling_factor_double;
     std::vector<Point_3> points;
     std::vector<std::array<int, 3>> faces;
--- test/test_tree.cpp
+++ test/test_tree.cpp
@@ -1,65 +1,50 @@
 #include "../include/json.hpp"
+#include "../include/json_helpers.hpp"
+#include "../include/query.hpp"
 #include "../include/tree.hpp"
 #include <catch2/catch_test_macros.hpp>
 #include <vector>
 
+#define EPSILON 1e-6
+
 TEST_CASE("Tree class", "[tree]") {
-    // Sample JSON data representing a tree
-    std::vector<nlohmann::json> jsonData = {
-        {{"id", 1},
-         {"lat", 37.7749},
-         {"lon", -122.4194},
-         {"genus", "Quercus"},
-         {"species", "rubra"},
-         {"height", 20.5},
-         {"circumference", 120.0},
-         {"diameter_crown", 8.0}},
-        {
-            {"id", 2},
-            {"lat", 37.7749},
-            {"lon", -122.4194},
-            {"genus", "Quercus"},
-            {"species", "rubra"},
-            {"height", 20.5},
-            {"circumference", 120.0},
-        },
-    };
+    perform_query("48.58715,7.75364,48.58731,7.75438");
+
+    nlohmann::json jsonData = get_query_result();
 
     // Create Tree objects from the JSON data
     auto treeLibrary = createLibraryFromJson(jsonData);
 
-    // Check the number of trees created
-    REQUIRE(treeLibrary.size() == 2);
-
     // Check the attributes of the first tree
-    REQUIRE(treeLibrary[0].id() == 1);
-    REQUIRE(treeLibrary[0].lat() == 37.7749);
-    REQUIRE(treeLibrary[0].lon() == -122.4194);
-    REQUIRE(treeLibrary[0].genus() == "Quercus");
-    REQUIRE(treeLibrary[0].species() == "rubra");
-    REQUIRE(treeLibrary[0].height() == 20.5);
-    REQUIRE(treeLibrary[0].circumference() == 120.0);
-    REQUIRE(treeLibrary[0].diameterCrown() == 8.0);
+    REQUIRE(treeLibrary[0].id() == 1572084575);
+    REQUIRE(treeLibrary[0].lat() == 48.5872656);
+    REQUIRE(treeLibrary[0].lon() == 7.7540658);
+    REQUIRE(treeLibrary[0].genus() == "Taxus");
+    REQUIRE(treeLibrary[0].species() == "Taxus baccata");
+    REQUIRE(treeLibrary[0].height() == 4);
+    REQUIRE(treeLibrary[0].circumference() == 1.5708);
+    REQUIRE(treeLibrary[0].diameterCrown() == 4);
 
     // Check the attributes of the second tree
-    REQUIRE(treeLibrary[1].id() == 2);
-    REQUIRE(treeLibrary[1].lat() == 37.7749);
-    REQUIRE(treeLibrary[1].lon() == -122.4194);
-    REQUIRE(treeLibrary[1].genus() == "Quercus");
-    REQUIRE(treeLibrary[1].species() == "rubra");
-    REQUIRE(treeLibrary[1].height() == 20.5);
-    REQUIRE(treeLibrary[1].circumference() == 120.0);
-    REQUIRE(treeLibrary[1].diameterCrown() == 0.0);
+    REQUIRE(treeLibrary[1].id() == 1572084576);
+    REQUIRE(treeLibrary[1].lat() == 48.5871975);
+    REQUIRE(treeLibrary[1].lon() == 7.7538365);
+    REQUIRE(treeLibrary[1].genus() == "Taxus");
+    REQUIRE(treeLibrary[1].species() == "Taxus baccata");
+    REQUIRE(treeLibrary[1].height() == 4);
+    REQUIRE(treeLibrary[1].circumference() == 0.91106);
+    REQUIRE(treeLibrary[1].diameterCrown() == 3);
 
     // Check the computed x and y coordinates of the first tree
-    treeLibrary[0].computeXY(37.7749, -122.4194);
-    REQUIRE(treeLibrary[0].x() == 0.0);
-    REQUIRE(treeLibrary[0].y() == 0.0);
+    treeLibrary[0].computeXY(48.5915821, 7.7566818);
+    REQUIRE(fabs(treeLibrary[0].x() + 192.9940319197) < EPSILON);
+    REQUIRE(fabs(treeLibrary[0].y() + 479.9991617497) < EPSILON);
 
     // Check the computed x and y coordinates of the second tree
-    treeLibrary[1].computeXY(37.7749, -122.4194);
+    treeLibrary[1].computeXY(48.5871975, 7.7538365);
     REQUIRE(treeLibrary[1].x() == 0.0);
+    REQUIRE(treeLibrary[1].y() == 0.0);
 
     // Check the wrap method
-    treeLibrary[0].wrap(0);
+    treeLibrary[0].wrap(0, "../../");
 }
