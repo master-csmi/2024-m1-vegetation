67a7da87b3ff3e3ad531726a3488505ff436a602
PA Senger <pierre.antoine.senger@gmail.com>
2024-05-24 10:16:05 +0200

    tried simply mesh

--- simplify_all.sh
+++ simplify_all.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Directory containing the raw STL files
-INPUT_DIR="tree_ref/raw_tree"
+INPUT_DIR="tree_ref"
 
 # Get the alpha value from the command line or set a default value
 if [ -z "$1" ]; then
@@ -12,5 +12,5 @@ fi
 
 # Loop through all STL files in the input directory
 for input_file in $INPUT_DIR/*.stl; do
-	./build/wrap "$input_file" "$ALPHA"
+	./build/simplify "$input_file" "$ALPHA"
 done
